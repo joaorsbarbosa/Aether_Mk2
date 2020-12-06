252fe130b8f41446eda531f7331652ae4a89a331
joaorsbarbosa <joaobarbosa96@gmail.com>
2020-12-06 13:53:21 +0000

    more refractoring

--- Aether_Mk2.py
+++ Aether_Mk2.py
@@ -1,5 +1,13 @@
 import os
 import sys
+from natsort import natsorted
 
 
-def sim_file_sorting
+def sim_file_sorting(file_extension, work_path):
+
+    list_of_files = [] 
+    for file in os.listdir(os.path.abspath(work_path)):
+        if file.endswith(file_extension):
+            list_of_files.append(file)
+
+    return natsorted(list_of_files)
